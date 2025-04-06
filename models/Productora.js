a530067381b0004f2757b6c35508e2cf8bff5750
Ebotero86 <edwin.botero@est.iudigital.edu.co>
2025-04-06 08:09:59 -0500

    Se realizan ajustes en el Backend

--- models/Productora.js
+++ models/Productora.js
@@ -3,10 +3,10 @@ const { Schema, model } = require ('mongoose');
 const ProductoraSchema = Schema({
    name: { type: String, require: true},
    state: { type: String, enum:['Activo', 'Inactivo'], require: true},
-   createdAt: { type: Date, require: true},
-   updatedAt: { type: String, require: true},
+   createdAt: { type: Date, required: true },
+   updatedAt: { type: Date, required: true },
    slogan: { type: String, required: false }, 
    description: { type: String, require: true},
 });
 
-module.exports = model ('Genero', ProductoraSchema);
+module.exports = model ('productora', ProductoraSchema);
